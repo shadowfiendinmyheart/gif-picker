76a0d95bebadc7033c4413886067b8db8e286679
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2022-03-28 22:46:51 +0300

    [add] css modules

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Im from "../Im";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Im />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Im/Im.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Im.module.css";
+
+function Im() {
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.blue}>Hello world!</span>
+    </div>
+  );
+}
+
+export default Im;
